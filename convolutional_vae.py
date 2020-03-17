082b04f5f0b202b40184db40c3ce6e4287d13782
Federico <federico.taschin@studenti.unipd.it>
2020-03-17 16:54:16 +0100

    Add beta to KL, separate model from training code
    Add beta parameter to model: now loss is reconstruction loss + beta * kl
    loss
    
    Separate model code from training loops with gradient tape and dataset

--- convolutional_vae.py
+++ convolutional_vae.py
@@ -1,15 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Flatten, Conv2DTranspose, Dense, Reshape
-import numpy as np
-from matplotlib import pyplot as plt
-import cv2
 
 
 class CVAE(tf.keras.Model):
 
-    def __init__(self, img_shape, latent_dim):
+    def __init__(self, img_shape, latent_dim, beta=1):
         """Create the Convolutional Variational Autoencoder model.
 
         The model consists of 4 convolutional layers, each one downscaling the image by a factor of 2, the encoded layer
@@ -23,6 +19,7 @@ class CVAE(tf.keras.Model):
         super(CVAE, self).__init__()
         self.img_shape = img_shape
         self.latent_dim = latent_dim
+        self.beta = beta
 
         encoder_input = Input(shape=self.img_shape)
         x = Conv2D(filters=32, kernel_size=5, activation='relu', padding='same')(encoder_input)
@@ -110,14 +107,13 @@ def compute_loss(model, x):
         Loss of the batch as described above
 
     """
-    print(type(x))
     mean, logvar = model.encode(x)
     z = model.reparametrize(mean, logvar)
     decoded = model.decode(z)
 
     reconstruction_loss = tf.reduce_mean(tf.reduce_sum(tf.square(x - decoded), axis=[1, 2, 3]))
     kl_loss = - tf.reduce_mean(0.5 * tf.reduce_sum(1 + logvar - tf.pow(mean, 2) - tf.exp(logvar), axis=1))
-    return reconstruction_loss + kl_loss
+    return reconstruction_loss + model.beta * kl_loss
 
 
 @tf.function
@@ -135,61 +131,3 @@ def compute_apply_gradients(model, x, opt):
     gradients = tape.gradient(loss, model.trainable_variables)
     opt.apply_gradients(zip(gradients, model.trainable_variables))
 
-
-def validate(model, validation_data):
-    n_validate = 100
-    sampled_indices = np.random.choice(range(len(validation_data)), n_validate)
-    sampled = validation_data[sampled_indices]
-    mean, logvar = model.encode(sampled)
-    decoded = model.decode(mean, apply_sigmoid=False)
-    return tf.keras.losses.mean_squared_error(sampled, decoded)
-
-
-def visualize_results(model, test_data, n_images=20):
-    images_per_row = 10
-    encoded = model.encode(test_data[0: n_images])
-    decoded = model.decode(encoded, apply_sigmoid=False)
-
-    true_images = test_images.reshape((-1, test_images.shape[1], test_images.shape[2]))
-    predicted_images = decoded.numpy().reshape((-1, test_images.shape[1], test_images.shape[2]))
-
-    f, axes = plt.subplots(int(n_images / images_per_row) * 2, images_per_row)
-    ax_idx = 0
-    for i in range(n_images):
-        if i % images_per_row == 0 and i > 0:
-            ax_idx += 2
-        axes[ax_idx, i % images_per_row].imshow(true_images[i])
-        axes[ax_idx + 1, i % images_per_row].imshow(predicted_images[i])
-    plt.show()
-
-
-if __name__ == "__main__":
-    img_dim = 64
-    channels = 1
-    (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
-    train_images = np.array([cv2.resize(img, (img_dim, img_dim)) for img in test_images])
-    test_images = np.array([cv2.resize(img, (img_dim, img_dim)) for img in test_images])
-
-    train_images = train_images.reshape(train_images.shape[0], img_dim, img_dim, channels).astype('float32')
-    test_images = test_images.reshape(test_images.shape[0], img_dim, img_dim, channels).astype('float32')
-
-    # Normalizing the images to the range of [0., 1.]
-    train_images /= 255.
-    test_images /= 255.
-
-    TRAIN_BUF = 60000
-    BATCH_SIZE = 100
-    TEST_BUF = 10000
-
-    optimizer = tf.keras.optimizers.Adam(1e-4)
-    autoencoder = CVAE(img_shape=(64, 64, 1), latent_dim=10)
-    epochs = 5
-    for epoch in range(epochs):
-        print("Epoch {}/{}".format(epoch, epochs))
-        bar = tf.keras.utils.Progbar(target=len(train_images), stateful_metrics=['validation_loss'])
-        for i in range(0, len(train_images), BATCH_SIZE):
-            compute_apply_gradients(autoencoder, train_images[i: i + BATCH_SIZE], optimizer)
-            val_loss = validate(autoencoder, test_images)
-            bar.add(BATCH_SIZE, [('validation_loss', val_loss)])
-
-    visualize_results(autoencoder, test_images, 20)
